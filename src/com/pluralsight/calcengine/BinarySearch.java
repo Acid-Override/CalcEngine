b91188657873bce35b35238f05ebf4674acb35f3
jupiter <Will.Atwood@lightwellinc.com>
2022-07-28 10:24:35 -0400

    Recursive Binary Search

--- src/com/pluralsight/calcengine/BinarySearch.java
+++ src/com/pluralsight/calcengine/BinarySearch.java
@@ -16,9 +16,34 @@ public class BinarySearch {
         }
         return arr[mid] == target ? mid : -1;
     }
-    public static int recursiveSearch(int[] arr, int target) {
-        //recursive code here
-        //more code here
-        return 21;
+    public static int recursiveSearch(int[] arr, int target, int min, int max) {
+        if ( max > min && min < arr.length ) {
+            int mid = (int) Math.floor((max + min) / 2 );
+
+            System.out.println(arr[mid]);
+
+            if ( arr[mid] == target) {
+                return mid;
+            }
+
+            if ( arr[mid] > target) {
+                return recursiveSearch(arr, target, min, mid - 1);
+            }
+            return recursiveSearch(arr, target, mid + 1, max);
+
+
+        }
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int[] testArr = new int[520000];
+        for (var i = 0; i < 520000; i++ ) {
+            testArr[i] = i;
+        }
+
+        search(testArr, 1);
+
+        recursiveSearch(testArr, 1, 0, testArr.length);
     }
 }
