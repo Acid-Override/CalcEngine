97a94712a95b0f496db92c6c77715dc62a04046f
Will Atwood <X1@galaxia.us>
2022-07-27 22:52:12 -0400

    Merge pull request #5
    * Search Again

--- src/com/pluralsight/calcengine/BinarySearch.java
+++ src/com/pluralsight/calcengine/BinarySearch.java
@@ -1,7 +1,19 @@
 package com.pluralsight.calcengine;
 
 public class BinarySearch {
-    public static int BinarySearch(int[] arr, int target) {
+    public static int search(int[] arr, int target) {
+        int min = 0, max = arr.length - 1;
+        int mid = (int) Math.floor( (max + min) / 2 );
 
+        while ( arr[mid] != target && min < max ) {
+            System.out.println(arr[mid]);
+            if ( arr[mid] > target ) {
+                max = mid + 1;
+            } else {
+                min = mid - 1;
+            }
+            mid = (int) Math.floor((max + min) / 2);
+        }
+        return arr[mid] == target ? mid : -1;
     }
 }
