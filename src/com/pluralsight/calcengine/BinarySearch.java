f6fc370a6e837d0d39e6b76213fa8fdf39888989
jupiter <Will.Atwood@lightwellinc.com>
2022-07-27 22:39:37 -0400

    BigSearch Again

--- src/com/pluralsight/calcengine/BinarySearch.java
+++ src/com/pluralsight/calcengine/BinarySearch.java
@@ -1,23 +1,7 @@
 package com.pluralsight.calcengine;
 
 public class BinarySearch {
-    public static int search(int[] arr, int target) {
-        int min = 0, max = arr.length - 1;
-        int mid = (int) Math.floor( (max + min) / 2 );
+    public static int BinarySearch(int[] arr, int target) {
 
-        while ( arr[mid] != target && min < max ) {
-            System.out.println(arr[mid]);
-            if ( arr[mid] > target ) {
-                max = mid + 1;
-            } else {
-                min = mid - 1;
-            }
-            mid = (int) Math.floor((max + min) / 2);
-        }
-        return arr[mid] == target ? mid : -1;
-    }
-    public static int recursiveSearch(int[] arr, int target) {
-        //recursive code here
-        //recursion is funnnnnnnn
     }
 }
