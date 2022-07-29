8b7e96fef634a9a485c9b0b4184356a54d84d3c1
jupiter <Will.Atwood@lightwellinc.com>
2022-07-28 23:47:52 -0400

    opCodeFromString and valueFromString

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -1,9 +1,12 @@
 package com.pluralsight.calcengine;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Factorial.factorial();
+        //Factorial.factorial();
         double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
         double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
         char[] opCodes = {'d', 'a', 's', 'm'};
@@ -14,12 +17,13 @@ public class Main {
         }
 
         if (args.length == 0 ) {
-            for (int i = 0; i < opCodes.length; i++){
-                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
-            }
-            for (double i : results) {
-                System.out.println(i);
-            }
+            executeInteractively();
+//            for (int i = 0; i < opCodes.length; i++){
+//                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
+//            }
+//            for (double i : results) {
+//                System.out.println(i);
+//            }
         } else if (args.length == 3) {
             //double result = execute ( args[0].charAt(0), Double.parseDouble(args[1]), Double.parseDouble(args[2]));
             double result = handleCommandLine(args);
@@ -32,12 +36,26 @@ public class Main {
         for (var i = 0; i < 520; i++ ) {
             testArr[i] = i;
         }
-        BinarySearch.search(testArr, 1);
+        //BinarySearch.search(testArr, 1);
 
         double[] totalAmount = CalculateInterest.produceInterestHistory(100d, .06d, 10);
 //        float total = (totalAmount[totalAmount.length - 1]).setScale(2, RoundingMode.CEILING);
         System.out.println("Total amount is : " + totalAmount[totalAmount.length - 1]);
+
     }
+
+    static void executeInteractively () {
+        System.out.println("Please enter a modifier and two numbers: ");
+        Scanner sc = new Scanner(System.in);
+        String arthmeticString = sc.nextLine();
+        String[] array = arthmeticString.split(" ");
+        char opCode = opCodeFromString(array[0]);
+        double leftVal = valueFromString(array[1]);
+        double rightVal = valueFromString(array[2]);
+        double result = execute(opCode, leftVal, rightVal);ad
+        System.out.println(array[0] + " " + array[1] + " & " + array[2] + " = " + result);
+    }
+
 
     private static double handleCommandLine(String[] args) {
         char opCode = args[0].charAt(0);
@@ -57,6 +75,20 @@ public class Main {
             default -> System.out.println("Invalid letter: " + opCode);
         }
         return result;
+    }
+    static char opCodeFromString(String operationName ){
+        return operationName.toLowerCase().charAt(0);
+    }
+    static double valueFromString(String str){
+        String[] stringOfValues = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
+        double value = 0d ;
+        for ( int i = 0; i < stringOfValues.length; i++ ) {
+            if ( str.equals(stringOfValues[i])) {
+                value = i;
+                break;
+            }
+        }
+        return value;
     }
 }
--- src/com/pluralsight/calcengine/StringEquality.java
+++ src/com/pluralsight/calcengine/StringEquality.java
@@ -23,11 +23,42 @@ public class StringEquality {
         String[] stringArr = str1.split(" ");
 
 
+        //Interning a string
+          //provides a canonicalized value
+          //enables reliable == operator comparison
+          //improves performance in case of frequently compared strings
+
+        String s3 = str1.intern();
+        String s4 = str2.intern();
+
+        if ( s4 == s3 ) {
+            System.out.println("THEY ARE EQUAL !!");
+        }
 
 
+        //String methods
+        //length
+        //Create : concat, replace, toLowerCase, toUpperCase, trim, split
+        //Extract: charAt, substring
+        //Test : contains, endsWith, startsWith, indexOf, lastIndexOf
+        //Comparison : equals, equalsIgnoreCase, isEmpty, compareTo, compareToIgnoreCase
+        //Formatting: format
+        //String conversion : valueOf
 
+        str1 = str1.concat("Ada Lovelace is a legend.");
+        System.out.println(str1);
+        if ( str1.contains("!")){
+        int index = str1.indexOf("!");
+        String sub = str1.substring(index + 1);
+        System.out.println(sub);
+        }
 
+        String strNum = String.valueOf(num);
+        System.out.println("My new string " + strNum);
 
+        //implicit conversion
+        int i = 4, j = 10;
+        System.out.println("My two numbers are : " + i + " & " + j + ".");
 
     }
 }
