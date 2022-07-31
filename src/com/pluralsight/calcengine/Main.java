9211dfd2d5e0ecea3f7ef6eb08953e8813a72592
jupiter <Will.Atwood@lightwellinc.com>
2022-07-31 01:30:28 -0400

    String.format update

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -1,59 +1,118 @@
 package com.pluralsight.calcengine;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        //Factorial.factorial();
-        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-        char[] opCodes = {'d', 'a', 's', 'm'};
-        double[] results = new double[opCodes.length];
+    public static void main(String[] zargs) {
+        System.out.println("Please enter a command or nothing for example calc. (ex: Multiply two nine or interactive or binary search)");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        String[] args = userInput.split(" ");
+
+
 
         for (String arg : args) {
             System.out.println(arg);
         }
 
-        if (args.length == 0 ) {
-            executeInteractively();
-//            for (int i = 0; i < opCodes.length; i++){
-//                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
-//            }
-//            for (double i : results) {
-//                System.out.println(i);
-//            }
+        if (args[0].equals("")) {
+            double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+            double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+            char[] opCodes = {'d', 'a', 's', 'm'};
+            double[] results = new double[opCodes.length];
+            for (int i = 0; i < opCodes.length; i++){
+                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
+            }
+            for (double i : results) {
+                System.out.println(i);
+            }
         } else if (args.length == 3) {
             //double result = execute ( args[0].charAt(0), Double.parseDouble(args[1]), Double.parseDouble(args[2]));
             double result = handleCommandLine(args);
             System.out.println(result);
-        } else {
+        } else if (args.length == 1 && args[0].equals("interactive")) {
+                executeInteractively();
+        } else if (args.length == 2 && args[0].equals("binary")) {
+            binaryInteractive();
+        }
+        else {
             System.out.println("Please provide an operation code and two numeric values.");
         }
 
-        int[] testArr = new int[520];
-        for (var i = 0; i < 520; i++ ) {
-            testArr[i] = i;
-        }
-        //BinarySearch.search(testArr, 1);
 
-        double[] totalAmount = CalculateInterest.produceInterestHistory(100d, .06d, 10);
+
+        //Factorial.factorial();
+        //double[] totalAmount = CalculateInterest.produceInterestHistory(100d, .06d, 10);
 //        float total = (totalAmount[totalAmount.length - 1]).setScale(2, RoundingMode.CEILING);
-        System.out.println("Total amount is : " + totalAmount[totalAmount.length - 1]);
+        //System.out.println("Total amount is : " + totalAmount[totalAmount.length - 1]);
 
+    }
+
+    private static void binaryInteractive() {
+        System.out.println("Please enter start, end and target and I will return the number of hops");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        String[] parts = userInput.split(" ");
+        int start = Integer.parseInt(parts[0]);
+        int end = Integer.parseInt(parts[1]);
+        int target = Integer.parseInt(parts[2]);
+
+        int total = end - start + 1;
+        int[] arr = new int[total];
+        for (int i = 0; i < total; i++ ) {
+            arr[i] = start++;
+        }
+        int searchResult = BinarySearch.search(arr, target);
+        System.out.println(searchResult);
+        int result = BinarySearch.recursiveSearch(arr, target, 0, arr.length - 1);
+        System.out.println("Recursive Binary Search : " + result);
     }
 
     static void executeInteractively () {
         System.out.println("Please enter a modifier and two numbers: ");
         Scanner sc = new Scanner(System.in);
-        String arthmeticString = sc.nextLine();
-        String[] array = arthmeticString.split(" ");
-        char opCode = opCodeFromString(array[0]);
-        double leftVal = valueFromString(array[1]);
-        double rightVal = valueFromString(array[2]);
-        double result = execute(opCode, leftVal, rightVal);ad
-        System.out.println(array[0] + " " + array[1] + " & " + array[2] + " = " + result);
+        String userInput = sc.nextLine(); //gather all input from user until they hit enter
+        String[] parts = userInput.split(" ");
+        performOperation(parts);
+        //System.out.println(parts[0] + " " + parts[1] + " & " + parts[2] + " = " + result);
+    }
+
+    private static void performOperation(String[] parts) {
+        char opCode = opCodeFromString(parts[0]);
+        double leftVal = valueFromString(parts[1]);
+        double rightVal = valueFromString(parts[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        displayResult(opCode, leftVal, rightVal, result);
+    }
+
+    private static void displayResult(char opCode, double leftVal, double rightVal, double result) {
+        char symbol = symbolFromOpCode(opCode);
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("The result of ");
+//        sb.append(leftVal);
+//        sb.append(symbol);
+//        sb.append(rightVal);
+//        sb.append(" is ");
+//        sb.append(result);
+//        String printOutResult = sb.toString();
+
+        String printOutResult = String.format("The result of %.1f %s %.1f is %.2f", leftVal, symbol, rightVal, result);
+
+        System.out.println(printOutResult);
+    }
+
+    private static char symbolFromOpCode(char opCode) {
+        char[] opCodes = {'a', 's', 'm', 'd'};
+        char[] symbols = {'+', '-', '*', '/'};
+        char symbol = ' ';
+
+        for ( int i = 0; i < opCodes.length; i++ ) {
+            if (opCode == opCodes[i]) {
+                symbol = symbols[i];
+                break;
+            }
+        }
+        return symbol;
     }
 
 
