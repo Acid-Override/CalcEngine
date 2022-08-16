fd21438533659fe53fa36db113bbdd63bc672875
jupiter <Will.Atwood@lightwellinc.com>
2022-08-16 18:53:52 -0400

    Junit Testing and Steams with reduce

--- src/com/pluralsight/calcengine/BinaryArrayToNumber.java
+++ src/com/pluralsight/calcengine/BinaryArrayToNumber.java
@@ -1,20 +1,24 @@
 package com.pluralsight.calcengine;
 
-//import java.util.Arrays;
+import java.util.Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BinaryArrayToNumber {
 
     public static void main(String[] args) {
-        System.out.println(convertBinaryArrayToNumber(new int[]{1, 0, 0, 1})); //11
+        System.out.println(convertBinaryArrayToNumber(new ArrayList<>( Arrays.asList(0, 1, 1, 0) ) )); //11
         System.out.println(convertBinaryArrayToInt(new int[]{1, 0, 0, 1}));
+        System.out.println(convertBinaryArrayToIntStreamAndReduce(Arrays.asList(0, 1, 1, 0) ));
     }
 
 
-    public static int convertBinaryArrayToNumber ( int[] arr ){
-        String[] strArray = new String[arr.length];
+    public static int convertBinaryArrayToNumber ( List<Integer> arr ){
+        String[] strArray = new String[arr.size()];
 
-        for ( int i = 0; i < arr.length; i++ ) {
-            strArray[i] = String.valueOf(arr[i]);
+        for ( int i = 0; i < arr.size(); i++ ) {
+            strArray[i] = String.valueOf(arr.get(i));
         }
         String strJoin = String.join("", strArray);
 
@@ -28,5 +32,11 @@ public class BinaryArrayToNumber {
         }
         return Integer.parseInt(sb.toString(), 2);
     }
+
+    public static int convertBinaryArrayToIntStreamAndReduce(List<Integer> binary) {
+        return binary.stream().reduce((x, y) -> x * 2 + y).get();
+
+    }
+
 
 }
