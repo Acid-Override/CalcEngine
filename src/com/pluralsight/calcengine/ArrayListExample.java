3e9602456bfeb385e08bc62d485c6114cf1b8723
Will Atwood <X1@galaxia.us>
2022-08-23 08:46:07 -0400

    Enum and ArrayList (#36)
    * ArrayList and Enum
    
    * added getter to Enum
    
    Co-authored-by: jupiter <Will.Atwood@lightwellinc.com>

--- src/com/pluralsight/calcengine/ArrayListExample.java
+++ src/com/pluralsight/calcengine/ArrayListExample.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class ArrayListExample {
 
     public static void main(String[] args) {
+
+
         ArrayList<String> testList = new ArrayList<>();
         testList.add("John");
         testList.add("Lily");
@@ -12,6 +14,7 @@ public class ArrayListExample {
         testList.add("Tony");
 
         testList.remove("Tony");
+        testList.set(2, "BoB");
 
         int size = testList.size();
         System.out.println(size);
