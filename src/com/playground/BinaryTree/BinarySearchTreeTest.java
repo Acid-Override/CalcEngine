6290fc8c5f18514cd3469480236dbbbfb9776417
WxAtwoo <Will.Atwood@mclaneco.com>
2023-01-10 17:36:51 -0500

    BST

--- src/com/playground/BinaryTree/BinarySearchTreeTest.java
+++ src/com/playground/BinaryTree/BinarySearchTreeTest.java
@@ -7,15 +7,65 @@ public class BinarySearchTreeTest {
     @Test
     void createANewRoot() {
         BinarySearchTree bst = new BinarySearchTree();
-        bst.add(bst.getRoot(), 10);
+        bst.insert(10);
         bst.toString();
     }
     @Test
     void createThreeNodesInCorrectOrder() {
         BinarySearchTree bst = new BinarySearchTree();
-        bst.add(bst.getRoot(), 10);
-        bst.add(bst.getRoot(), 5);
-        bst.add(bst.getRoot(), 15);
-        bst.toString();
+        bst.insert(10);
+        bst.insert(5);
+        bst.insert(15);
+
+        System.out.println(bst.toString());
+    }
+    @Test
+    void createFourNodesUsingRecursion() {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(10);
+        bst.insert(5);
+        bst.insert(15);
+        bst.insert(7);
+        bst.printValues();
+        System.out.println(bst.toString());
+    }
+
+    @Test
+    void printAllValuesInorder() {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(50);
+        bst.insert(40);
+        bst.insert(60);
+        bst.insert(45);
+        bst.insert(30);
+        bst.insert(35);
+        bst.insert(55);
+        bst.insert(70);
+        bst.insert(65);
+        bst.printValues();
+        System.out.println(bst.toString());
+    }
+    @Test
+    void insertAValue(){
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(50);
+        bst.insert(40);
+        bst.insert(60);
+        bst.printValues();
+        System.out.println(bst.toString());
+    }
+    @Test
+    void findAValue() {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(50);
+        bst.insert(40);
+        bst.insert(60);
+        bst.insert(45);
+        bst.insert(30);
+        bst.insert(35);
+        bst.insert(55);
+        bst.insert(70);
+        bst.insert(65);
+        bst.search(35);
     }
 }
