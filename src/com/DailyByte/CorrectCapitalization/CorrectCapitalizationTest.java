7126aa76bc83dd7d1c0116ad38059566c0fd2573
WxAtwoo <Will.Atwood@mclaneco.com>
2023-02-15 12:46:52 -0500

    Adding new tests and log to CorrectCapitalizationTest

--- src/com/DailyByte/CorrectCapitalization/CorrectCapitalizationTest.java
+++ src/com/DailyByte/CorrectCapitalization/CorrectCapitalizationTest.java
@@ -1,14 +1,43 @@
 package com.DailyByte.CorrectCapitalization;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.logging.Logger;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CorrectCapitalizationTest {
 
-    @Test
-    void correctCapitalizationTest() {
-        CorrectCapitalization cc = new CorrectCapitalization();
-        cc.correctCapitalization("HEllO");
+    private Logger log = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    private CorrectCapitalization cc;
+
+
+    @BeforeAll
+    void init() {
+        log.info("Startup");
+        cc = new CorrectCapitalization();
     }
+
+    @Test
+    void correctAllCapitalizationTest() {
+        assertTrue(cc.detectCapitalUse("HELLO"));
+    }
+    @Test
+    void correctFirstLetterCapitalizationTest() {
+        assertTrue(cc.detectCapitalUse("Supercalifragaligistic"));
+    }
+    @Test
+    void incorrectCapitalLetterPlacement() {
+        assertFalse(cc.detectCapitalUse("suPercalifragaligistic"));
+    }
+    @AfterAll
+    void teardown() {
+        log.info("TearDown");
+    }
+
 }
